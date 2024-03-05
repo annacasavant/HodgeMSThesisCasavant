6eba49d7751333f9e8f51b19c2009b72b368b3bb
prao7 <prao7@wisc.edu>
2024-03-05 13:45:34 -0700

    added in basic framework

--- testProductionCostModel.jl
+++ testProductionCostModel.jl
@@ -3,7 +3,11 @@ This is a test Production Cost Model using PowerSystems.jl and PowerSimulations.
 The following test is based off the demonstration by Dr. Kate Doubleday of the Global 
 Power Systems Transformation Consortium.
 
-The example can be found using this link: https://www.youtube.com/watch?v=84IQjMe_CPs&pp=ygUaa2F0ZSBkb3VibGVkYXkgbnJlbCBzaWVubmE%3D
+The walkthrough for the example can be found using this link: 
+https://www.youtube.com/watch?v=84IQjMe_CPs&pp=ygUaa2F0ZSBkb3VibGVkYXkgbnJlbCBzaWVubmE%3D
+
+The walkthrough has been modified using the documented example in PowerSimulations.jl:
+https://nrel-sienna.github.io/PowerSimulations.jl/latest/tutorials/pcm_simulation/
 
 @author: Anna Casavant and Pradyumna Rao
 """
@@ -17,5 +21,10 @@ using Dates
 using CSV
 using DataFrames
 using HiGHS
-solver = optimizer_with_attributes(HiGHS.Optimizer)
+solver = optimizer_with_attributes(HiGHS.Optimizer, "mip_rel_gap" => 0.5)
+MathOptInterface.OptimizerWithAttributes(HiGHS.Optimizer, Pair{MathOptInterface.AbstractOptimizerAttribute, Any}[MathOptInterface.RawOptimizerAttribute("mip_rel_gap") => 0.5])
 plotlyjs()
+
+
+# Import System
+#sys = System() Need to find a dataset for the system
